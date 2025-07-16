7fb5dd6138b0776438667470f4cb3b1e0828ea25
Riya Singh <537riya@gmail.com>
2025-07-16 16:55:54 +0530

    Add files till date

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
+
+const HomePage = () => {
+
+    const [selectedUser, setSelectedUser] = useState(false)
+
+  return (
+     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]' > {/*background image*/} 
+      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
+        <Sidebar />
+        <ChatContainer />
+        <RightSideBar />
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
